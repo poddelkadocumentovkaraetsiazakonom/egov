7d957a015c50064f7dc82249c8b9e2267e0fe297
poddelkadocumentovkaraetsiazakonom385ukrk <145637021+poddelkadocumentovkaraetsiazakonom@users.noreply.github.com>
2026-09-08 14:19:57 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'egov-v2';
-const STATIC_ASSETS = [
+const CACHE_NAME = 'egov-v1';
+const ASSETS = [
   './',
   './index.html',
   './document.html',
@@ -7,43 +7,17 @@ const STATIC_ASSETS = [
   './app.js',
   './manifest.json',
   './my-id.jpg',
-  './id-card.png',
-  './app-icon.png',
-  'https://cdn.jsdelivr.net/npm/qrcodejs/qrcode.min.js'
+  './app-icon.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      );
-    })
-  );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        fetch(e.request).then((networkResponse) => {
-          if (networkResponse.status === 200) {
-            caches.open(CACHE_NAME).then((cache) => cache.put(e.request, networkResponse));
-          }
-        }).catch(() => {});
-        return cachedResponse;
-      }
-      return fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
